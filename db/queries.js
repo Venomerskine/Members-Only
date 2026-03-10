19d06bdb809fc6712eb8d13f7f235a0da3884b4e
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-03-10 19:28:22 +0300

    created the query file and a get messages query

---
+++ db/queries.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const pool =  require("./index")
+
+async function getAllMessages() {
+    const query = `
+        select 
+            u.id as userId,
+            u.first_name,
+            u.last_name ,
+            u.username,
+            u.email ,
+            u."role" ,
+            m.id  as messageId,
+            m.title ,
+            m.boy_text ,
+            m.created_at ,
+            m.user_id as messageUserId
+        from users u 
+        join messages m 
+        on m.user_id = u.id 
+    `
+}
