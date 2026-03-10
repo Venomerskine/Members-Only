ce6f6489a25d1b922bde5826affae48d80810c2b
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-03-10 08:26:58 +0300

    created the routes to the create and delete message

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const memberController = require("../controllers/memberController")
 const passport = require('passport');
-const {isAuthenticated} = require("../middleware/authMiddleware")
+const {isAuthenticated, requireRole} = require("../middleware/authMiddleware")
 
 router.get("/", memberController.getMemberAuth)
 router.get("/dashboard",isAuthenticated ,memberController.loginSuccess)
@@ -17,7 +17,8 @@ router.post(
 }))
 
 router.post("/join", isAuthenticated, memberController.joinClub)
-
 router.post("/logout", memberController.logout)
+router.post("/messages", requireRole("member", "admin"), memberController.createMessage);
+router.delete("/messages/:id", requireRole("admin"), memberController.deleteMessage);
 
 module.exports = router;
