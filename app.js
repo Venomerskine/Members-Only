a08dfb31cf6a56f63c8c0ec2ae54aab311c426c6
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-03-12 07:07:16 +0300

    implemented the css

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'pulblic')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
